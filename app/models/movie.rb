4982f0a506e38e99296add10558a839306dbb015
tzequi-og <146213960+tzequi-og@users.noreply.github.com>
2023-11-06 16:53:24 +0000

    movie_director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,12 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+
+  def director
+    my_director_id = self.director_id
+    matching_directors = Director.where({ :id => my_director_id})
+    the_director = matching_directors.first
+    return the_director
+  end
+
 end
